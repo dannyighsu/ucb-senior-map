6dc23b564831e58d1f7fcef9402c1d83bb60363f
josephli036 <josephli036@gmail.com>
2016-04-15 03:57:22 +0000

    added edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < Devise::RegistrationsController
+  
+  protect_from_forgery with: :exception
+
+    before_filter :configure_permitted_parameters, if: :devise_controller?
+    
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :school, :description, :location, :email, :password) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :school, :description, :location, :email, :password, :current_password) }
+    end
 
   def new
     @user = User.new
-  end
-  
-  def edit
-    @user = current_user
   end
 
   def create
