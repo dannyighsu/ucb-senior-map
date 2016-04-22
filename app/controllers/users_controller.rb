db8a2a6e2b8d37bfc1c04480056a960b69120251
Lisang Zhou <Death,2013>
2016-04-21 22:42:03 -0700

    added a tab info box for major and included in the map, also some styling (font) and pins too

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < Devise::RegistrationsController
     before_filter :configure_permitted_parameters, if: :devise_controller?
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :school, :description, :location, :email, :password) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :school, :description, :location, :email, :password, :current_password) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :school, :description, :location, :email, :password, :major) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :school, :description, :location, :email, :password, :current_password, :major) }
     end
 
   def new
@@ -45,11 +45,16 @@ class UsersController < Devise::RegistrationsController
         flash[:notice] = "Your description is too long."
         redirect_to new_user_registration_path
         return
+      elsif user[:major] != nil && user[:major].length > 10
+        flash[:notice] = "Your major is too long."
+        redirect_to new_user_registration_path
+        return
       end
 
       resource.first_name = user[:first_name]
       resource.last_name = user[:last_name]
       resource.location = user[:location]
+      resource.major = user[:major]
       resource.school = user[:school]
       resource.description = user[:description]
       result = resource.save
--- db/migrate/20160415000426_add_school_and_description_columns_to_users.rb
+++ db/migrate/20160415000426_add_school_and_description_columns_to_users.rb
@@ -2,11 +2,15 @@ class AddSchoolAndDescriptionColumnsToUsers < ActiveRecord::Migration
   def up
     add_column :users, :school, :string
     add_column :users, :description, :text
+    add_column :users, :major, :text
+
   end
 
   def down
     remove_column :users, :school
     remove_column :users, :description
+    remove_column :users, :major
+
   end
-  
+
 end
