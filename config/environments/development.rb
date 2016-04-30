793c0f875909d4a07fdc33565cf3ebf3833de017
Danny Hsu <dannyighsu@gmail.com>
2016-04-29 23:41:52 -0700

    Implemented JS location lookup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "smtp.gmail.com", :port => 587, :domain => "gmail.com", :user_name => "ucbclassmap@gmail.com", :password => "berkeleyseniorcouncil", :authentication => "plain" }
+  config.action_mailer.smtp_settings = {:address => "smtp.gmail.com", :port => 587, :domain => "gmail.com", :user_name => ENV["Email_Username"], :password => ENV["Email_Password"], :authentication => "plain" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'ucb-senior-map.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "smtp.gmail.com", :port => 587, :domain => "gmail.com", :user_name => ENV["Mailer_Username"], :password => ENV["Mailer_Password"], :authentication => "plain" }
+  config.action_mailer.smtp_settings = {:address => "smtp.gmail.com", :port => 587, :domain => "gmail.com", :user_name => ENV["Email_Username"], :password => ENV["Email_Password"], :authentication => "plain" }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
