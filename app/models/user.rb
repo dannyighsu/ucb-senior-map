88e3bcc0b589e822a1f89a0a0f52f317ca217cb3
Danny Hsu <dannyighsu@gmail.com>
2016-05-02 14:00:14 -0700

    Added back development.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  before_create :confirmation_token
-
-  def confirmation_token
-    if self.confirm_token.blank?
-      self.confirm_token = SecureRandom.urlsafe_base64.to_s
-    end
-  end
 end
